995fd096c28d221e5396a06b1b95956fa86337d8
Press03img <pressss03@gmail.com>
2025-02-04 15:03:42 +0900

    Update ASME_BPVC_II-D_M.py

--- ASME_BPVC_II-D_M.py
+++ ASME_BPVC_II-D_M.py
@@ -62,8 +62,8 @@ if not filtered_df.empty:
     st.markdown(
         detail_data.style.set_table_styles([
             {"selector": "table", "props": [("width", "100%"), ("table-layout", "fixed")]},
-            {"selector": "th", "props": [("text-align", "center"), ("width", "50%")]},
-            {"selector": "td:nth-child(2)", "props": [("text-align", "center"), ("width", "50%")]},
+            {"selector": "th", "props": [("text-align", "center")]},
+            {"selector": "td:nth-child(2)", "props": [("text-align", "center")]},
         ]).hide(axis="index").to_html(),
         unsafe_allow_html=True
     )
